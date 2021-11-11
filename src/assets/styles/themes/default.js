bd3be6c14c1d87a2082f65e602670ec4c5856e55
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-10 22:39:51 -0300

    style: add theme default

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,12 @@
+export default {
+  backgroundColor: {
+    main: '#0F0F0F',
+    sunny: '#56CBDB',
+    rainy: '#5E6675',
+    snowy: '#CFCFCF',
+  },
+  colors: {
+    white: '#fff',
+    black: '#000',
+  },
+};
