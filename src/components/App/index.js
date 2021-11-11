1c83fd7dea1b008032165b2194a1a70154ecf747
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-11 16:11:00 -0300

    feat: component App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,16 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
+import { Container } from './styles';
+import Home from '../../pages/Home';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <div>
-        Hello World
-      </div>
+      <Container>
+        <Home />
+      </Container>
     </ThemeProvider>
   );
 }
