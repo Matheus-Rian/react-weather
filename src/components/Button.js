cd5a770820084522a2c4b6c00b1b602858728640
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-11 16:12:12 -0300

    style: styles Button

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  border: none;
+  background: none;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 24px;
+  padding: 8px 12px;
+  margin: 8px 4px;
+  transition: all 0.2s ease-in;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.white};
+    color: ${({ theme }) => theme.colors.black};
+  }
+`;
