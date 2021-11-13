e8e6da23b0d4b9ab6a53f5139e8d84ad805d8701
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-13 20:50:46 -0300

    feat: add page Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import { Container } from './styles';
+import { citys } from '../../utils/citys';
+import world from '../../assets/images/world.svg';
+import ButtonCity from '../../components/Button';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>weather</h1>
+      <h2>select a city</h2>
+      <img src={world} alt="World" height={200} width={100} />
+      <div>
+        {citys.map((city) => (
+          <ButtonCity key={city}>
+            {' '}
+            {city}
+          </ButtonCity>
+        ))}
+      </div>
+    </Container>
+  );
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1, h2 {
+    font-weight: 300;
+  }
+
+  h1 {
+    text-transform: uppercase;
+    letter-spacing: 1px;
+  }
+
+  h2 {
+    color: ${({ theme }) => theme.colors.gray}
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 75%;
+
+    @media(max-width: 425px) {
+      width: 80%;
+
+      button {
+        font-size: 2rem;
+      }
+    }
+
+    @media(max-width: 355px) {
+      width: 95%;
+    }
+  }
+`;
