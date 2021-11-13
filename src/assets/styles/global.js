364826740a25517abe018920b1dac86456b57726
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-13 20:48:19 -0300

    style: adjust styles global

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -5,15 +5,23 @@ export default createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  html,
+  body {
     font-family: 'Advent Pro', sans-serif;
+    height: 100vh;
   }
 
   body {
     background: ${({ theme }) => theme.backgroundColor.main};
     color: ${({ theme }) => theme.colors.white};
-    font-size: 16px;
+    font-size: 1.6rem;
   }
-
   button {
     cursor: pointer;
   }
