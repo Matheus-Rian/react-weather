dc6fa0a9300af1a40fd65e41942a9cd4304b297c
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-13 20:49:06 -0300

    style: adjust styles Container in App

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-  max-width: 500px;
   margin: 0 auto;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
 `;
