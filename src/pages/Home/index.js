0a24310f3dc9e3651773c9d70509734b10f61a35
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-15 18:13:19 -0300

    feat: add navigate to page weather city

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,11 @@ import { citys } from '../../utils/citys';
 import world from '../../assets/images/world.svg';
 import ButtonCity from '../../components/Button';
 
-export default function Home() {
+export default function Home({ history }) {
+  function handleCity(city) {
+    history.push(`/weather/${city}`);
+  }
+
   return (
     <Container>
       <h1>weather</h1>
@@ -11,7 +15,7 @@ export default function Home() {
       <img src={world} alt="World" height={200} width={100} />
       <div>
         {citys.map((city) => (
-          <ButtonCity key={city}>
+          <ButtonCity key={city} city={city} onCity={handleCity}>
             {' '}
             {city}
           </ButtonCity>
