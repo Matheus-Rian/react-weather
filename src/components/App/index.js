c4017be0f2af738e5b05f2358692a3462598c2f1
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-15 18:18:01 -0300

    feat: add routes app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,20 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
-import Home from '../../pages/Home';
+import Routes from '../../Routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Home />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
