249665d97f7c02e538c5cedc93c49af2127b1d32
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-15 18:20:36 -0300

    feat: integration with api weather

--- src/pages/WeatherCity/index.js
+++ src/pages/WeatherCity/index.js
@@ -1,37 +1,63 @@
-import { Ancora, Container } from './styles';
-import rain from '../../assets/images/rain.svg';
+import { Back, Container } from './styles';
 import arrow from '../../assets/images/arrow.svg';
 import WeatherDay from '../../components/WeatherDay';
 import WeatherMoreInformations from '../../components/WeatherMoreInformations';
+import useFetch from '../../hooks/useFetch';
 
-export default function WeatherCity() {
+export default function WeatherCity({ history }) {
+  const city = history.location.pathname.split('/');
+  const [loading, response] = useFetch(`&q=${city[city.length - 1]}&aqi=no`);
+
+  function handleBack() {
+    history.goBack();
+  }
+
+  if (loading) { return <h1>Carregando...</h1>; }
+
   return (
     <>
-      <Ancora className="link-back" to="/">
-        <img src={arrow} alt="Voltar" />
-        <span>Voltar</span>
-      </Ancora>
+      <Back onClick={handleBack} className="link-back">
+        <button type="button">
+          <img src={arrow} alt="Voltar" width={30} height={30} />
+        </button>
+      </Back>
       <Container>
         <div className="main-information">
-          <h1>London</h1>
-          <h2>rainy</h2>
+          <h1>{response?.data.location.name}</h1>
+          <h2>{response?.data.forecast.forecastday[0].day.condition.text}</h2>
           <div className="temp">
-            <p>17</p>
+            <p>{response?.data.current.temp_c}</p>
             <div className="arrows">
               <img src={arrow} alt="arrow-top" />
               <img src={arrow} alt="arrow-bottom" />
             </div>
             <div>
               <span>ºC</span>
-              <p>{`${0}º`}</p>
-              <p>-8º</p>
+              <p>{`${response?.data.forecast.forecastday[0].day.maxtemp_c}º`}</p>
+              <p>{`${response?.data.forecast.forecastday[0].day.mintemp_c}º`}</p>
             </div>
           </div>
-          <img src={rain} alt="rain" width={100} height={100} />
+          <img
+            src={response?.data.current.condition.icon}
+            alt={response?.data.current.condition.text}
+            width={150}
+            height={130}
+          />
         </div>
 
-        <WeatherDay />
-        <WeatherMoreInformations />
+        <WeatherDay
+          dawn={response?.data.forecast.forecastday[0].hour[3]}
+          morning={response?.data.forecast.forecastday[0].hour[9]}
+          afternoon={response?.data.forecast.forecastday[0].hour[15]}
+          night={response?.data.forecast.forecastday[0].hour[21]}
+        />
+
+        <WeatherMoreInformations
+          windSpeed={response?.data.current.wind_mph}
+          sunrise={response?.data.forecast.forecastday[0].astro.sunrise}
+          sunset={response?.data.forecast.forecastday[0].astro.sunset}
+          humidity={response?.data.current.humidity}
+        />
       </Container>
     </>
   );
