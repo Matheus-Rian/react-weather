7a837b98bf5c0f1c1d4c98294ab50bb0181f406a
Matheus Rian <souzamatheusrian@gmail.com>
2021-11-17 21:23:21 -0300

    refactor: remove test app

--- src/components/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from '.';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const title = getByText('Hello World');
-  expect(title).toBeInTheDocument();
-});
